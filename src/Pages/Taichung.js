004a2d567830ff8b3b52631d891a77d299198e11
Yangunli <yangun410@gmail.com>
2022-09-30 15:25:10 +0800

    Update Style

--- src/Pages/Taichung.js
+++ src/Pages/Taichung.js
@@ -22,7 +22,7 @@ const Taichung = () => {
         xs={9}
         lg={10}
         md={10}
-        sx={{ marginTop: { xs: "6rem", sm: "8rem", md: "10rem", lg: "10rem" } }}
+        sx={{ marginTop: { xs: "8rem", sm: "9rem", md: "10rem", lg: "10rem" } }}
       >
         <Grid container spacing={5}>
           {taichungExp.map((exp) => (
--- src/Pages/Tainan.js
+++ src/Pages/Tainan.js
@@ -21,7 +21,7 @@ const Tainan = () => {
         xs={9}
         lg={10}
         md={10}
-        sx={{ marginTop: { xs: "6rem", sm: "8rem", md: "10rem", lg: "10rem" } }}
+        sx={{ marginTop: { xs: "8rem", sm: "9rem", md: "10rem", lg: "10rem" } }}
       >
         <Grid container spacing={5}>
           {tainanExp.map((exp, i) => (
